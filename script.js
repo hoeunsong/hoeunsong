23e3be96c4a7345738ce9012212546c8f3eb432f
hoeunsong <she0661@naver.com>
2025-07-08 17:03:53 +0900

    최종: 무한 연속 슬라이더 적용 & 전체 레이아웃 완성

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
+// SSLIDER 자동은 CSS, JS 불필요
 // 폼 제출 → thankyou.html
 document.getElementById('consult-form').addEventListener('submit', function(e) {
   e.preventDefault();
-  // TODO: 구글 시트 연동 로직 추가 가능
   window.location.href = 'thankyou.html';
 });
