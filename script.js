4987f5baf6c565ce5944f8eae8b834890c71db51
hoeunsong <she0661@naver.com>
2025-07-09 11:38:06 +0900

    Formspree 연동용 form action/method 추가, 슬라이더 애니메이션 유지

--- script.js
+++ script.js
@@ -1,22 +1,3 @@
-document.getElementById('consult-form').addEventListener('submit', function(e) {
-  e.preventDefault();
-
-  const name  = document.getElementById('name').value;
-  const phone = document.getElementById('phone').value;
-  const type  = document.getElementById('type').value;
-
-  fetch('https://script.google.com/macros/s/AKfycbzea5M4Pm05apWhpPLW-y5ZbeSoEWlhYR1jTjAKdAqewcev-XC2cb1kHCjDXCTjHc56pQ/exec', {
-    method: 'POST',
-    contentType: 'application/json',
-    body: JSON.stringify({ name, phone, type })
-  })
-  .then(res => res.json())
-  .then(json => {
-    if (json.status === 'success') {
-      window.location.href = 'thankyou.html';
-    } else {
-      alert('제출 실패: ' + json.message);
-    }
-  })
-  .catch(err => alert('통신 에러: ' + err));
-});
+// (기존 슬라이더 애니메이션만 수행)
+console.log("▶ script.js 로딩됨");
+// 다른 로직이 없다면 빈 파일로 두셔도 무방합니다.
