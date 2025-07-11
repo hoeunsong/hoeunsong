29d62ab643cf4d6f0254a557aae2d9ec16689d8b
hoeunsong <she0661@naver.com>
2025-07-11 12:54:32 +0900

    폼 제출 오류 수정 및 submit.php 연결 경로 변경

--- submit.php
+++ submit.php
@@ -1,4 +1,6 @@
 <?php
+header("Access-Control-Allow-Origin: *");
+
 $name = $_POST['name'] ?? '';
 $carrier = $_POST['carrier'] ?? '';
 $phone1 = $_POST['phone1'] ?? '';
@@ -6,13 +8,9 @@ $phone2 = $_POST['phone2'] ?? '';
 $phone3 = $_POST['phone3'] ?? '';
 $type = $_POST['type'] ?? '';
 
-// 전화번호 조합
 $phone = $phone1 . '-' . $phone2 . '-' . $phone3;
 
-// 로그 형식 지정 (줄바꿈 추가)
-$log = "이름: $name\n통신사: $carrier\n연락처: $phone\n보험유형: $type\n\n";
-
-// 로그 파일에 저장
+$log = "이름: $name, 통신사: $carrier, 연락처: $phone, 보험유형: $type\n";
 file_put_contents("data.txt", $log, FILE_APPEND);
 
 echo "ok";
